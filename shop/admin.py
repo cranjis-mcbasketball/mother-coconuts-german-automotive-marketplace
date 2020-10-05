ef3cef8b0175d5fc5ac35c10c02eb5b2357a2fd5
Mason Borchard <masonborchard@gmail.com>
2020-10-05 15:21:54 -0700

    remove Archive dir from github repo

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import *
-# Register models
+# Register models (always do this after updating columns)
 
 admin.site.register(User)
 admin.site.register(Company)
